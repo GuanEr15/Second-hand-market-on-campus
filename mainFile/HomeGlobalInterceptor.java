b5bb7cf9cc2f801b4dfed8192a28945ac64691a0
GuanEr <guaner15@163.com>
2022-02-13 14:15:12 +0800

    edit HomeGlobalInterceptor.java

---
+++ mainFile/HomeGlobalInterceptor.java
@@ -0,0 +1,48 @@
+package com.guaner.interceptor.home;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.guaner.config.SiteConfig;
+import com.guaner.entity.common.SiteSetting;
+import com.guaner.service.common.FriendLinkService;
+import com.guaner.service.common.GoodsCategoryService;
+import com.guaner.service.common.SiteSettingService;
+import com.guaner.util.StringUtil;
+
+/**
+ * 前台全局拦截器
+ * @author Administrator
+ *
+ */
+@Component
+public class HomeGlobalInterceptor implements HandlerInterceptor{
+
+	@Autowired
+	private GoodsCategoryService goodsCategoryService;
+	@Autowired
+	private SiteConfig siteConfig;
+	@Autowired
+	private FriendLinkService friendLinkService;
+	@Autowired
+	private SiteSettingService siteSettingService;
+	@Override
+	public boolean  preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
+		
+		if(!StringUtil.isAjax(request)){
+			//若不是ajax请求，则将菜单信息放入页面模板变量
+			request.setAttribute("goodsCategorys", goodsCategoryService.findAll());
+			request.setAttribute("friendLinkList", friendLinkService.findList(8));
+			SiteSetting siteSetting = siteSettingService.find();
+			if(siteSetting != null){
+				request.setAttribute("siteName", siteSetting.getSiteName());
+				request.setAttribute("siteSetting", siteSetting);
+			}
+		}
+		return true;
+	}
+}
