1ec087245b17ea40c1a68e2de3bd4b54df5fd254
GuanEr15 <guaner15@163.com>
2023-06-21 09:37:17 +0800

    #202306210937

--- src/main/java/com/guaner/listener/SessionListener.java
+++ src/main/java/com/guaner/listener/SessionListener.java
@@ -1,27 +1,29 @@
 package com.guaner.listener;
+
 import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpSessionEvent;
-/**
- * session监听类，用于监听session的创建，销毁
- */
 import javax.servlet.http.HttpSessionListener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+/**
+ * session监听类，用于监听session的创建，销毁
+ */
 @WebListener
 public class SessionListener implements HttpSessionListener {
-	
+
 	private Logger log = LoggerFactory.getLogger(SessionListener.class);
-	
+
 	public static long onlineUserCount = 0;
-	
+
 	@Override
-	public void sessionCreated(HttpSessionEvent se){
+	public void sessionCreated(HttpSessionEvent se) {
 		log.info("进入session创建事件！当前在线用户数：" + (++onlineUserCount));
 	}
-	
+
 	@Override
-	public void sessionDestroyed(HttpSessionEvent se){
+	public void sessionDestroyed(HttpSessionEvent se) {
 		log.info("进入session销毁事件！当前在线用户数：" + (--onlineUserCount));
 	}
 }
--- src/main/java/com/guaner/service/admin/DatabaseBakService.java
+++ src/main/java/com/guaner/service/admin/DatabaseBakService.java
@@ -1,4 +1,5 @@
 package com.guaner.service.admin;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
@@ -29,70 +30,79 @@ public class DatabaseBakService {
 
 	@Resource
 	private com.guaner.service.admin.OperaterLogService operaterLogService;
+
 	@Resource
 	private DatabaseBakDao databaseBakDao;
+
 	@Value("${guaner.database.backup.dir}")
 	private String backUpDir;
+
 	@Value("${guaner.database.backup.username}")
 	private String dbUsername;
+
 	@Value("${guaner.database.backup.password}")
 	private String dbPwd;
+
 	@Value("${guaner.database.backup.database.name}")
 	private String dbName;
-	
+
 	private Logger log = LoggerFactory.getLogger(DatabaseBakService.class);
-	
+
 	/**
 	 * 分页查找数据库备份记录
+	 * 
 	 * @param pageBean
 	 * @return
 	 */
-	public PageBean<DatabaseBak> findList(PageBean<DatabaseBak> pageBean){
-		Pageable pageable = PageRequest.of(pageBean.getCurrentPage()-1, pageBean.getPageSize());
+	public PageBean<DatabaseBak> findList(PageBean<DatabaseBak> pageBean) {
+		Pageable pageable = PageRequest.of(pageBean.getCurrentPage() - 1, pageBean.getPageSize());
 		Page<DatabaseBak> findAll = databaseBakDao.findAll(pageable);
 		pageBean.setContent(findAll.getContent());
 		pageBean.setTotal(findAll.getTotalElements());
 		pageBean.setTotalPage(findAll.getTotalPages());
 		return pageBean;
 	}
-	
+
 	/**
 	 * 添加或修改数据库备份记录
+	 * 
 	 * @param databaseBak
 	 * @return
 	 */
-	public DatabaseBak save(DatabaseBak databaseBak){
+	public DatabaseBak save(DatabaseBak databaseBak) {
 		return databaseBakDao.save(databaseBak);
 	}
-	
+
 	/**
 	 * 根据id查询
+	 * 
 	 * @param id
 	 * @return
 	 */
-	public DatabaseBak find(Long id){
+	public DatabaseBak find(Long id) {
 		return databaseBakDao.find(id);
 	}
-	
+
 	/**
 	 * 根据id删除
+	 * 
 	 * @param id
 	 */
-	public void delete(Long id){
+	public void delete(Long id) {
 		databaseBakDao.deleteById(id);
 	}
-	
+
 	/**
 	 * 备份数据库
 	 */
-	public void backup(){
+	public void backup() {
 		File path = new File(backUpDir);
-		if(!path.exists()){
+		if (!path.exists()) {
 			path.mkdir();
 		}
 		try {
 			String filename = dbName + "_" + StringUtil.getFormatterDate(new Date(), "yyyyMMddHHmmss") + ".sql";
-			String cmd = "mysqldump -u"+dbUsername+" -p"+dbPwd+" "+dbName+" -r " + backUpDir + filename;
+			String cmd = "mysqldump -u" + dbUsername + " -p" + dbPwd + " " + dbName + " -r " + backUpDir + filename;
 			Runtime.getRuntime().exec(cmd);
 			DatabaseBak databaseBak = new DatabaseBak();
 			databaseBak.setFilename(filename);
@@ -105,22 +115,25 @@ public class DatabaseBakService {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * 还原数据库
+	 * 
 	 * @param id
 	 */
-	public void restore(Long id){
+	public void restore(Long id) {
 		DatabaseBak databaseBak = find(id);
-		if(databaseBak != null){
+		if (databaseBak != null) {
 			try {
 				String filename = databaseBak.getFilename();
 				File file = new File(databaseBak.getFilepath() + databaseBak.getFilename());
-				String cmd = "mysql -u"+dbUsername+" -p"+dbPwd+" "+dbName+" < " + backUpDir + filename;;
-				if(!file.exists()){
-					cmd = "mysql -u"+dbUsername+" -p"+dbPwd+" "+dbName+" < " + databaseBak.getFilepath() + databaseBak.getFilename();
+				String cmd = "mysql -u" + dbUsername + " -p" + dbPwd + " " + dbName + " < " + backUpDir + filename;
+				;
+				if (!file.exists()) {
+					cmd = "mysql -u" + dbUsername + " -p" + dbPwd + " " + dbName + " < " + databaseBak.getFilepath()
+							+ databaseBak.getFilename();
 				}
-				String stmt1 = "mysqladmin -u "+dbUsername+" -p"+dbPwd+" create "+dbName;
+				String stmt1 = "mysqladmin -u " + dbUsername + " -p" + dbPwd + " create " + dbName;
 				String[] cmds = { "cmd", "/c", cmd };
 				Runtime.getRuntime().exec(stmt1);
 				Process exec = Runtime.getRuntime().exec(cmds);
@@ -133,12 +146,13 @@ public class DatabaseBakService {
 			}
 		}
 	}
-	
+
 	/**
 	 * 备份总数
+	 * 
 	 * @return
 	 */
-	public long total(){
+	public long total() {
 		return databaseBakDao.count();
 	}
 }
